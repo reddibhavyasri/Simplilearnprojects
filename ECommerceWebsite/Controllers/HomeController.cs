972aaeb7ca49e5e77c2b3f7ada0427b234466622
reddibhavyasri <reddibhavyasri@gmail.com>
2022-11-03 14:54:38 +0530

    push

---
+++ ECommerceWebsite/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using ECommerceWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        EcommerceEntities2 db = new EcommerceEntities2();
+        public ActionResult Index()
+        {
+            return View(db.ProductTables.OrderByDescending(x => x.prodid).ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+    }
+}
