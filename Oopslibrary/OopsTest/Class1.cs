f3a6764779355e128e21c707cc7d4f9ddc1cd036
reddibhavyasri <reddibhavyasri@gmail.com>
2022-11-06 16:20:11 +0530

    push

---
+++ Oopslibrary/OopsTest/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Oopslibrary;
+using NUnit.Framework;
+namespace oop_class_test
+{
+    [TestFixture]
+    public class Class_test
+    {
+        /// </summary>
+        [TestCase]
+        public void test_student()
+        {
+            Student s = new Student();
+           
+            s.SName = "Pavs";
+            Assert.AreEqual(s.SName, "Pavs");
+        }
+        [TestCase]
+        public void test_subject()
+        {
+            Subject s = new Subject();
+           
+            s.SubName = "physics";
+          
+            Assert.AreEqual(s.SubName, "Physics");
+        }
+        [TestCase]
+        public void test_teacher()
+        {
+            Teacher s = new Teacher();
+            
+            s.TName = "Renu";
+            Assert.AreEqual(s.TName, "Renu");
+        }
+
+    }
+}
---
+++ Oopslibrary/Oopslibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oopslibrary
+{
+   
+    
+        public interface Interface1
+        {
+            void PrintData();
+
+
+        }
+    }
+
