9bb2de23a7e8aee2a34c5f8530312a39aa372dee
reddibhavyasri <reddibhavyasri@gmail.com>
2022-11-06 16:17:23 +0530

    push

---
+++ bankproject/Loginpage/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loginpage.Models;
+
+namespace Loginpage.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public List<UserModel> PutValue()
+        {
+            var users = new List<UserModel>
+            {
+                new UserModel{id=1,username="12345",password="renu"},
+                new UserModel{id=2,username="12444",password="priya"},
+                new UserModel{id=3,username="24666",password="pavs"},
+                new UserModel{id=4,username="24444",password="pavs12"}
+            };
+
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Verify(UserModel usr)
+        {
+            var u = PutValue();
+
+            var ue = u.Where(u => u.username.Equals(usr.username));
+
+            var up = ue.Where(p => p.password.Equals(usr.password));
+
+            if(up.Count()==1)
+            {
+                ViewBag.message = "Login Success";
+                return View("LoginSuccess");
+            }
+            else
+            {
+                ViewBag.message = "Login Failed";
+                return View("Login");
+            }
+        }
+    }
+}
